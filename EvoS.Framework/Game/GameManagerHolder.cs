66f025cedb2f485ed3f7d00c5ee1d36a7f72e384
LimitlessRegret <55678532+LimitlessRegret@users.noreply.github.com>
2019-12-22 19:46:40 +1100

    GameManagerHolder: Add stub to create a GameManager instance from LobbyGame and TeamInfo

--- EvoS.Framework/Game/GameManagerHolder.cs
+++ EvoS.Framework/Game/GameManagerHolder.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using EvoS.Framework.Constants.Enums;
 using EvoS.Framework.Misc;
 using EvoS.Framework.Network.Game.Messages;
@@ -40,6 +41,34 @@ namespace EvoS.Framework.Game
             }
 
             return _gameManagers[loginRequest.AccountId];
+        }
+
+        public static void CreateMatch(LobbyGameInfo gameInfo, LobbyServerTeamInfo teamInfo)
+        {
+            var game = new GameManager();
+            // TODO setup GameManager instance
+            game.SetTeamInfo(teamInfo);
+            game.SetGameInfo(new LobbyGameInfo
+            {
+                GameConfig = new LobbyGameConfig
+                {
+                    Map = "VR_Practice"
+                    // Map = "CargoShip_Deathmatch"
+                    // Map = "Casino01_Deathmatch"
+                    // Map = "EvosLab_Deathmatch"
+                    // Map = "Oblivion_Deathmatch"
+                    // Map = "Reactor_Deathmatch"
+                    // Map = "RobotFactory_Deathmatch"
+                    // Map = "Skyway_Deathmatch"
+                }
+            });
+            game.LaunchGame();
+            
+            foreach (var playerInfo in teamInfo.TeamAPlayerInfo.Union(teamInfo.TeamPlayerInfo))
+            {
+                
+                _gameManagers.Add(playerInfo.AccountId.ToString(), game);
+            }
         }
     }
 }
