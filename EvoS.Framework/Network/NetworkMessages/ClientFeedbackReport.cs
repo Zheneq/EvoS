8630d5f51fc30d896f686fbbdb5a996ff0488dfc
Addalyn <dev@addalyn.baby>
2023-02-15 00:07:37 +0100

    Improvement Send user reports to an discord webhook Able to report a player and or send feedback bugs/suggestions

--- EvoS.Framework/Network/NetworkMessages/ClientFeedbackReport.cs
+++ EvoS.Framework/Network/NetworkMessages/ClientFeedbackReport.cs
@@ -18,18 +18,18 @@ namespace EvoS.Framework.Network.NetworkMessages
         [EvosMessage(688, typeof(ClientFeedbackReport.FeedbackReason))]
         public enum FeedbackReason
         {
-			Suggestion,
-			Bug,
-			u0012,
-			u0015,
-			u0016,
-			u0013,
-			u0018,
-			u0009,
-			u0019,
-			u0011,
-			u001A,
-			u0004
-		}
+            _,
+            Suggestion,
+            Bug,
+            UnsportsmanlikeConduct,
+            Harassment,
+            AFKing,
+            HateSpeech,
+            Feeding,
+            Spam,
+            OffensiveName,
+            Other,
+            Botting
+        }
     }
 }
